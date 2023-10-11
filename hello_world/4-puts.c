e77a8a699f87b2119816c602286b1d1a0c6216f7
Puddimax <maxiaguilar@gmail.com>
2023-10-11 08:57:21 -0700

    puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<studio.h>
+/*
+ *Manin - exercise 4
+ *
+ *Description: using puts
+ *
+ *Return: nothing
+ */
+int main(void) {
+      puts("Programming is like building a multilingual puzzle")
+      return 0;
+}      
