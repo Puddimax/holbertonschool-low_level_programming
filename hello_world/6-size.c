759cfebbffcdf20919184019fe35bcc760cbedc8
Puddimax <maxiaguilar@gmail.com>
2023-10-11 10:21:41 -0700

    sizeof

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -2,7 +2,7 @@
 /**
  * main - exercise
  *
- * Description: using printf
+ * Description: using sizeof
  *
  * Return: nothing
  */
@@ -11,8 +11,8 @@ int main(void)
 {
 printf("Size of char: %zu bytes\n", sizeof(char));
 printf("Size of int: %zu bytes\n", sizeof(int));
-printf("Size of long: %zu bytes\n", sizeof(long int));
-printf("Size of long long: %zu bytes\n", sizeof(long long int));
+printf("Size of long int: %zu bytes\n", sizeof(long int));
+printf("Size of long long int: %zu bytes\n", sizeof(long long int));
 printf("Size of float: %zu bytes\n", sizeof(float));
 return (0);
 }
