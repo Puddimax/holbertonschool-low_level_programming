32a073b505b61f9b8db0fcdc6efb444798d1e9bd
Puddimax <maxiaguilar@gmail.com>
2023-10-11 09:49:12 -0700

    printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - exercise
+ *
+ * Description: using printf
+ *
+ * Return: nothing
+ */
+
+int main()
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return 0;
+}
