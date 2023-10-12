89cef3200a5ea21f5a5ffa23f7ab39e80c71d241
Puddimax <maxiaguilar@gmail.com>
2023-10-12 09:34:57 -0700

    q e

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: AlphabetL
+ *
+ * Return: zero for success
+ */
+
+int main(void)
+	{
+	char letterL = 'a';
+
+	while (letterL <= 'z' && letterL != "q" "e")
+	{
+	putchar(letterL);
+	letterL++;
+	}
+	putchar('\n');
+	return (0);
+	}
