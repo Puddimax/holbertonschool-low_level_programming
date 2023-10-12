56eb74193177d1788a94aa7aafaf6ba341e56b17
Puddimax <maxiaguilar@gmail.com>
2023-10-12 10:14:18 -0700

    number

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -11,13 +11,16 @@
 
 int main(void)
 	{
-	int i = 0;
-
-	while (i <= 9)
-	{
-	putchar("%d", i);
-	i++;
-	}
+	putchar('0');
+	putchar('1');
+	putchar('2');
+	putchar('3');
+	putchar('4');
+	putchar('5');
+	putchar('6');
+	putchar('7');
+	putchar('8');
+	putchar('9');
 	putchar('\n');
 	return (0);
 	}
