8a071f6feebbc4f84f025372d70994313fde3524
Puddimax <maxiaguilar@gmail.com>
2023-10-12 10:07:50 -0700

    numb2

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Base ten
+ *
+ * Return: zero for success
+ */
+
+int main(void)
+	{
+	int i = 0;
+
+	while (i <= 9)
+	{
+	printf("%d", i);
+	i++;
+	}
+	putchar('\n');
+	return (0);
+	}
