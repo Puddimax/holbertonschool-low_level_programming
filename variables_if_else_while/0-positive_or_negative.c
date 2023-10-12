e178cbd3d6ed27bd507f4236df7c02939cef8211
Puddimax <maxiaguilar@gmail.com>
2023-10-12 06:25:26 -0700

    num

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,17 +10,21 @@
  */
 
 int main(void)
-{
+	{
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n > 0) {
-	printf("is positive.\n");
-} 	else if (n < 0) {
-	printf("is negative.\n");
-}	else {
-	printf("is zero.\n");
-}
+	if (n > 0)
+	{
+		printf("is positive.\n");
+	}
+	if (n < 0)
+	{
+		printf("is negative.\n");
+	}
+	if
+	{
+		printf("is zero.\n");
+	}
 	return (0);
-}
+	}
