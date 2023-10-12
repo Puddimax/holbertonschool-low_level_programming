0bb59c1e36eb2fbfa7a87a56dc4a8c37d9e5c25e
Puddimax <maxiaguilar@gmail.com>
2023-10-12 08:33:25 -0700

    alphaUL

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Alphabet
+ *
+ * Return: zero for success
+ */
+
+int main(void)
+        {
+        char letterL = 'a';
+	char letterU = 'A';
+
+        while (letterL <= 'z')
+        {
+        putchar(letterL);
+        letterL++;
+        }
+	while (letterU <= 'Z')
+	{
+	putchar(letterU);
+	letterU++;
+	}
+        putchar('\n');
+        return (0);
+        }
