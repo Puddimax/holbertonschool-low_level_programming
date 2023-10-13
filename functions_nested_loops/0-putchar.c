bdd3d00ee9bb8105a6f0aafc5c0212d7c43514e4
Puddimax <maxiaguilar@gmail.com>
2023-10-13 09:33:28 -0700

    put

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Functions
+ *
+ * Return: zero for success
+ */
+
+int main()
+	{
+	printf("_putchar\n");
+	return 0;
+	}
