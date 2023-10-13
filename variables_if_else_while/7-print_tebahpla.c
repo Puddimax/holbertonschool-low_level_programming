fe4a73e604a74269361bc60ece2af8ab3a820a88
Puddimax <maxiaguilar@gmail.com>
2023-10-13 06:10:36 -0700

    rever

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ * Description: Base ten
+ *
+ * Return: zero for success
+ */
+
+int main(void)
+	{
+	char c
+
+	for (c = 0; c <= 'z'; c--);
+	{
+	putchar(c);
+	}
+	putchar('\n');
+	return (0);
+	}
