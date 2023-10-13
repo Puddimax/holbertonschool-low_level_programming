ab317cb89b611323302f84f23a6c15ff6d169b2e
Puddimax <maxiaguilar@gmail.com>
2023-10-13 11:16:42 -0700

    3

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * int _islower - exercise
+ *
+ * Description: Functions alphabet
+ *
+ * Return: 1 for lowercase, 0 for other
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z') 
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+    	}
+}
