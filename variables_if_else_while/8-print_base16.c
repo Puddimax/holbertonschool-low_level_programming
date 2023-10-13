3e8fbe06b24c238e8d9b0e4d87d50e17178fec08
Puddimax <maxiaguilar@gmail.com>
2023-10-13 06:30:25 -0700

    base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Base ten
+ *
+ * Return: zero for success
+ */
+
+int main(void)
+        {
+	int i;
+
+	while (i = '0'; i >= '0' && i < '16'; i++)
+	{
+	putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+	}
