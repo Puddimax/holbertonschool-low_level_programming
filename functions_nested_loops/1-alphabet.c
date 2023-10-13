34e5bee3ffad0a88c095ceb6806d64f061e31a46
Puddimax <maxiaguilar@gmail.com>
2023-10-13 09:50:11 -0700

    1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Functions
+ *
+ * Return: zero for success
+ */
+
+int main(void)
+	{
+	while (print_alphabet <= 'z')
+	{
+	putchar(print_alphabet);
+	print_alphabet++;
+	}
+	putchar('\n');
+	return (0);
+	}
+	
