af7e6937d0f269fd5f636e8dfd3aea4b43ba8566
Puddimax <maxiaguilar@gmail.com>
2023-10-13 07:17:36 -0700

    n,

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -13,10 +13,12 @@ int main(void)
 	{
 	int i;
 
-	for (i = '0'; i >= '0' && i <= '9'; i++)
+	for (i = 0; i >= 0 && i <= 9; i++)
 	{
 	putchar(i + '0');
-	putchar(", ");
+	putchar(',');
+	putchar(' ');
 	}
+	putchar('\n');
 	return (0);
 	}
