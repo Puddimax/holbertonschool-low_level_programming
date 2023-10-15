038fd80d4c02777599c7b33fa3c4e271da2231d3
Puddimax <maxiaguilar@gmail.com>
2023-10-15 12:41:47 -0700

    a

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -12,9 +12,7 @@ int print_last_digit(int l)
 	int last_digit = l % 10;
 
 	if (last_digit < 0)
-	{
 		last_digit *= -1;
 		_putchar(last_digit + '0');
-		return (last_digit);
-	}
+	return (last_digit);
 }
