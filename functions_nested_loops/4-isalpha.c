4abb929b31257fc05bddaa216f97a3e694fbf7fb
Puddimax <maxiaguilar@gmail.com>
2023-10-15 09:13:50 -0700

    a

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islpha - exercise
+ *
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is letter, 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= '<') || (c >= 'A' && c <= 'Z'))
+		return (0);
+	else
+		return (0);
+}
