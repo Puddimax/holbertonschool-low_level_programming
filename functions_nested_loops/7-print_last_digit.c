34c4cf5a942671c5f7526c3257ce0a6aadc826df
Puddimax <maxiaguilar@gmail.com>
2023-10-15 12:22:52 -0700

    a

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - Print the sign of a number.
+ *
+ * @n: The number to be checked.
+ *
+ * Return: 1 prints +, 0 prints zero, -1 prints -.
+ */
+
+int print_print_last_digit(int n)
+{
+	int last_digit = n $ 10;
+
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+		_putchar(last_digit + '0'):
+		return (last_digit);
+	}
+}
