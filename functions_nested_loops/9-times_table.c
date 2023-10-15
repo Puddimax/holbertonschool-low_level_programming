175f5051b862246879ed507ac3fb08916e82d84a
Puddimax <maxiaguilar@gmail.com>
2023-10-15 14:35:27 -0700

    a

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -27,12 +27,15 @@ void times_table(void)
 			}
 
 			if (prod <= 9)
+			{
 				_putchar(' ');
+				_putchar(' ');
+			}
 			else
+			{
 				_putchar((prod / 10) + '0');
-
-			_putchar((prod % 10) + '0');
-	}
+				_putchar((prod % 10) + '0');
+			}
+		}
 		_putchar('\n');
-	}
 }
