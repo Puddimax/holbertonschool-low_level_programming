195a7a6b6a1017d294d9e7ad4ce5e9e686255879
Puddimax <maxiaguilar@gmail.com>
2023-10-15 15:08:03 -0700

    a

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Print the sign of a number.
+ *
+ * @n: The number to be checked.
+ *
+ * Return: 1 prints +, 0 prints zero, -1 prints -.
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
