054dbfbdaa5022b2d6c386c989c503110ccff0e3
Puddimax <maxiaguilar@gmail.com>
2023-10-15 10:07:07 -0700

    a

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - exercise
+ *
+ * @n: The number to be checked.
+ *
+ * Return: 1 Always 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
