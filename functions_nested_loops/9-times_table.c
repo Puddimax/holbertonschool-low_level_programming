d156a89c01ecf3dace460c4df2155eb58fde5a2f
Puddimax <maxiaguilar@gmail.com>
2023-10-15 14:15:00 -0700

    a

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,20 +14,24 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		for (mult = 0; mult <= 9; mult++)
-	{
+		{
 			prod = num * mult;
+
 			if (mult > 0)
 			{
 				_putchar(',');
 				_putchar(' ');
+
 				if (prod <= 9)
 					_putchar(' ');
 			}
+
 			if (prod <= 9)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+
+			_putchar((prod % 10) + '0');
 	}
 		_putchar('\n');
 	}
