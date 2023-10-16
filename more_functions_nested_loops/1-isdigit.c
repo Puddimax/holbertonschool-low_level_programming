f3ea2f868f09792c868d093eae6006643650628a
Puddimax <maxiaguilar@gmail.com>
2023-10-15 19:10:00 -0700

    a

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * isdigit - Checks for uppercase characters.
+ * _isdigit - Checks for uppercase characters.
  *
  * @c: The character to be checked.
  *
- * Return: 1 if character is uppercase, 0 otherwise.
+ * Return: 1 if number is a digit, 0 otherwise
  */
 
 int _isdigit(int c)
